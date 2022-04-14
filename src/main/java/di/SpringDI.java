5e9940a4f03949aa1c3bf9bcc84bfbd706cc60b6
Barmaley <e.g.zakharov@gmail.com>
2022-04-14 14:03:24 +0300

    2. Сканирование проекта [#1070]

--- src/main/java/di/SpringDI.java
+++ src/main/java/di/SpringDI.java
@@ -4,15 +4,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class SpringDI {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(Store.class);
-        context.register(StartUI.class);
-        context.register(ConsoleInput.class);
-        context.refresh();
-        StartUI ui = context.getBean(StartUI.class);
-        ui.add("Petr Arsentev");
-        ui.add("Ivan ivanov");
-        ui.ask("Ask ");
-        ui.print();
+        try (
+                AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext()
+        ) {
+            context.scan("di");
+            context.refresh();
+            StartUI ui = context.getBean(StartUI.class);
+            ui.add("Petr Arsentev");
+            ui.add("Ivan ivanov");
+            ui.print();
+            ui.ask("Ask ");
+        }
     }
 }
--- src/main/java/di/StartUI.java
+++ src/main/java/di/StartUI.java
@@ -1,5 +1,8 @@
 package di;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class StartUI {
 
     private final Store store;
